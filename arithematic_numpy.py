af80ff902943f184ac9eaf74421fab4835fddfa6
syeda ilha <syedailha17@gmail.com>
2026-06-14 19:05:28 +0530

    Add subtraction to numpy operations
    Added subtraction operation and clarified rounding functions.

--- arithematic_numpy.py
+++ arithematic_numpy.py
@@ -2,6 +2,7 @@ import numpy as np
 array=np.array([1,2,3])
 print(array+1)
 print(array**3)
+print(array-1)
 print(np.sqrt(array))
 #to simply round the number use round function just like sqrt in line 5.to round up use ceil and to round down use floor
 print(np.pi)
